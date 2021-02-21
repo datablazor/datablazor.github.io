12bb0f1196cd5470d977dccee02132dbdbee09bf
Sunil Raj Thota <sunilrajt.128@gmail.com>
2021-02-21 01:54:38 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -104,12 +104,12 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/blog`,
-        name: `blog`,
-      },
+//     {
+//       resolve: `gatsby-source-filesystem`,
+//       options: {
+//         path: `${__dirname}/content/blog`,
+//         name: `blog`,
+//       },
     },
     {
       resolve: `gatsby-transformer-remark`,
