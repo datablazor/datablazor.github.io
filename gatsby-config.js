6db25b6a4362f67d07892c705f08d01d8e0bdbae
Sunil Raj Thota <sunilrajt.128@gmail.com>
2021-02-23 18:14:05 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -141,7 +141,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: `ADD YOUR TRACKING ID HERE`, // Optional Google Analytics
+        trackingId: `UA-106320620-1`, // Optional Google Analytics
       },
     },
     {
